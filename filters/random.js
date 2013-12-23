5f8d9739b466d7e16d0fd403dfa85310682d06c0
vomitcuddle <shinku@dollbooru.org>
2013-12-23 10:59:43 +0000

    Add random filter

---
+++ filters/random.js
@@ -0,0 +1,16 @@
+// filters/random.js - Tweet filter based on a biased dice roll.
+// This file is part of maidbot.
+// Copyright (c) 2013 vomitcuddle <shinku@dollbooru.org>
+// License: MIT
+
+/**
+ * Filters tweets based on random probability.
+ * @param {Number} weight Probability that tweet will be randomly selected. (1-100)
+ * @returns {Boolean} Returns true (1/weight)*100 percent of the time.
+ */
+function random (weight) {
+  return Math.ceil(Math.random()*100) < weight;
+}
+
+// Export module.
+module.exports = random;
