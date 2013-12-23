a56c0760bd1227b22f93d70de472a57af68aae1b
vomitcuddle <shinku@dollbooru.org>
2013-12-23 11:40:58 +0000

    Add regexp filter

---
+++ filters/regexp.js
@@ -0,0 +1,21 @@
+// filters/regexp.js - FIlters tweets based on regular expressions.
+// This file is part of maidbot.
+// Copyright (c) 2013 vomitcuddle <shinku@dollbooru.org>
+// License: MIT
+
+/**
+ * Filters tweets based on regular expressions.
+ * @param {String} regex Regular expression string.
+ * @param {Object} tweet Tweet to filter.
+ * @return {Boolean} True if tweet matches the regular expression.
+ */
+function regexp (regex, tweet) {
+  // Compile regexp if necessary.
+  if (!(regex instanceof RegExp)) {
+    regex = new RegExp(regex);
+  }
+  return regex.test(tweet.text);
+}
+
+// Export module.
+module.exports = regexp;
