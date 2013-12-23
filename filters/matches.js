dfc989c0c05aa89eafead11c6dda80a78493e700
vomitcuddle <shinku@dollbooru.org>
2013-12-23 12:09:58 +0000

    Add matches filter

---
+++ filters/matches.js
@@ -0,0 +1,16 @@
+// filters/matches.js - Looks for substring in tweets.
+// This file is part of maidbot.
+// Copyright (c) vomitcuddle <shinku@dollbooru.org>
+// License: MIT
+
+/**
+ * Checks if tweet contains the given substring.
+ * @param {String} s Substring to look for.
+ * @returns {Boolean} True if tweet contains given substring.
+ */
+function matches (s, tweet) {
+  return tweet.text.indexOf(s) > -1;
+}
+
+// Export module.
+module.exports = matches;
