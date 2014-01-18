d04dc8e57f9ce6cc78c88de90fab5351ed582507
vomitcuddle <shinku@dollbooru.org>
2014-01-18 16:27:57 +0000

    Remove old mock twit stuff

--- test/core/config.js
+++ test/core/config.js
@@ -7,28 +7,28 @@ var should = require('should');
 /** Configuration loader tests. */
 describe('Configuration loader', function () {
   it('returns an error if file does not exist', function (done) {
-    config.load('test/mock/idontexist.json', function (error) {
+    config.load('test/examples/idontexist.json', function (error) {
       should.exist(error);
       error.message.should.equal('Configuration file does not exist');
       done();
     });
   });
   it('returns an error if file is not valid JSON', function (done) {
-    config.load('test/mock/invalid.json', function (error) {
+    config.load('test/examples/invalid.json', function (error) {
       should.exist(error);
       error.message.should.match(/^Error parsing configuration file:/);
       done();
     });
   });
   it('returns an error if file contains no JSON nodes', function (done) {
-    config.load('test/mock/empty.json', function (error) {
+    config.load('test/examples/empty.json', function (error) {
       should.exist(error);
       error.message.should.equal('Error parsing configuration file:\nUnexpected end of input');
       done();
     });
   });
   it('returns configuration as parsed object', function (done) {
-    config.load('test/mock/maidbot.json', function (error, config) {
+    config.load('test/examples/maidbot.json', function (error, config) {
       should.not.exist(error);
       config.should.be.an.instanceOf(Object);
       (function() {
