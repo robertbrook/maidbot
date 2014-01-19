2e7e0d6869720aa916525d09f20bc7210149bc8a
vomitcuddle <shinku@dollbooru.org>
2014-01-19 20:43:14 +0000

    Create filters/index.js

---
+++ filters/index.js
@@ -0,0 +1,11 @@
+// This file is part of maidbot.
+// Copyright (c) 2014 vomitcuddle <shinku@dollbooru.org>
+// License: MIT.
+
+// Export filters.
+module.exports = {
+  matches: require('./matches'),
+  random: require('./random'),
+  regexp: require('./regexp'),
+  userid: require('./userid')
+};
